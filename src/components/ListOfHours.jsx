7595d6ded7c75c57d41970d42bcc77eed81bd3ab
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-29 15:32:10 -0300

    feat (hours): implement new feature of hours by day

--- src/components/ListOfHours.jsx
+++ src/components/ListOfHours.jsx
@@ -6,16 +6,20 @@ export default function ListOfHours() {
   const { horarios, turnoData, handleTurnoData } = useStore();
   return (
     <div className=" grid grid-cols-4 gap-2  ">
-      {horarios.map((time) => (
-        <TimeCard
-          key={time.hs}
-          disabled={turnoData.barberId === ""}
-          time={time}
-          selected={turnoData.time === time.hs}
-          handleTime={handleTurnoData}
-          className="p-2 "
-        />
-      ))}
+      {horarios ? (
+        horarios.map((time) => (
+          <TimeCard
+            key={time.hs}
+            disabled={turnoData.barberId === ""}
+            time={time}
+            selected={turnoData.time === time.hs}
+            handleTime={handleTurnoData}
+            className="p-2 "
+          />
+        ))
+      ) : (
+        <span>No disponible</span>
+      )}
     </div>
   );
 }
