201733cf65b9a0326cab0c2dcdf6d029b4096785
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-01-15 17:22:07 -0300

    feat (error): add message alert when no barbers render

--- src/components/SelectBarber.jsx
+++ src/components/SelectBarber.jsx
@@ -4,19 +4,21 @@ import ListOfBarbers from "./ListOfBarbers";
 import { useStore } from "../context/BarberContext";
 import Loader from "./Loader";
 
-export default function SelectBarber({ client }) {
+export default function SelectBarber() {
   const { barberos, handleTurnoData } = useStore();
   return (
     <FormLayout
       className=" grid place-items-center h-[90%] "
       title={"Peluquero"}
     >
-      {barberos.length ? (
-        <ListOfBarbers
+      {barberos ? (
+        barberos.length === 0 ?
+        <span className="bg-error p-4 rounded-md text-white font-semibold ">No hay peluqueros disponibles en este momento</span>
+        :<ListOfBarbers
           className={"flex flex-col gap-2 w-[60%] m-auto   "}
           barberos={barberos}
         />
-      ) : (
+      ) :  (
         <Loader />
       )}
     </FormLayout>
