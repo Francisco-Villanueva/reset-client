55de24bab25fd42655e69ed36e3e0991f69c28a4
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-01-15 17:33:58 -0300

    testing env

--- src/services/index.js
+++ src/services/index.js
@@ -1,32 +1,31 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
-const API_KEY = import.meta.env.VITE_API_KEY;
 export class ApiServices {
   static async getAllBarbers() {
-    return await axios.get(`${API_URL}/barbers/active/?API_KEY=${API_KEY}`);
+    return await axios.get(`${API_URL}/barbers/active/`);
   }
   static async setTurno(data) {
-    return await axios.post(`${API_URL}/appointment/?API_KEY=${API_KEY}`, data);
+    return await axios.post(`${API_URL}/appointment/`, data);
   }
   static async getHorarios(barberId, date) {
     return await axios.get(
-      `${API_URL}/hours/${barberId}/${date}/?API_KEY=${API_KEY}`
+      `${API_URL}/hours/${barberId}/${date}/`
     );
   }
   static async getTurnoById(turnoId) {
     return await axios.get(
-      `${API_URL}/appointment/${turnoId}/?API_KEY=${API_KEY}`
+      `${API_URL}/appointment/${turnoId}/`
     );
   }
   static async deletTurno(turnoId) {
     return await axios.delete(
-      `${API_URL}/appointment/${turnoId}/?API_KEY=${API_KEY}`
+      `${API_URL}/appointment/${turnoId}/`
     );
   }
   static async getBarberById(barberId) {
     return await axios.get(
-      `${API_URL}/barbers/${barberId}/?API_KEY=${API_KEY}`
+      `${API_URL}/barbers/${barberId}/`
     );
   }
 }
