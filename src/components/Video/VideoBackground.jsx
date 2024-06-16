eac9b6df9cf5bdfa4983702a049f3c30c2cea79e
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-06-16 19:20:44 -0300

    fix ui vido sizing

--- src/components/Video/VideoBackground.jsx
+++ src/components/Video/VideoBackground.jsx
@@ -6,15 +6,17 @@ import "./video.css";
 // Crea un componente funcional para el video
 const VideoBackground = ({ videoSrc }) => {
   return (
-    <div className="video-container">
-      <ReactPlayer
-        url={videoSrc}
-        width="100%"
-        height="100%"
-        loop
-        muted
-        playing
-      />
+    <div className="">
+      <div>
+        <ReactPlayer
+          url={videoSrc}
+          width="100%"
+          height="100%"
+          loop
+          muted
+          playing
+        />
+      </div>
       {/* <video autoPlay loop muted className="video">
         <source src={videoSrc} type="video/mp4" />
       </video> */}
