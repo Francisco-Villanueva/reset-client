cd2e5056cf4dcc5bae872998f4ab99e55710f757
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-06-16 19:17:34 -0300

    added react player

--- src/components/Video/VideoBackground.jsx
+++ src/components/Video/VideoBackground.jsx
@@ -1,42 +1,23 @@
 // Importa React
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import ReactPlayer from "react-player";
 import "./video.css";
 
 // Crea un componente funcional para el video
 const VideoBackground = ({ videoSrc }) => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const videoElement = videoRef.current;
-
-    const playVideo = () => {
-      if (videoElement) {
-        const playPromise = videoElement.play();
-        if (playPromise !== undefined) {
-          playPromise
-            .then(() => {
-              // El video se reproduce automáticamente
-            })
-            .catch((error) => {
-              // Reintentar la reproducción
-              videoElement.muted = true;
-              videoElement.play();
-            });
-        }
-      }
-    };
-
-    playVideo();
-
-    window.addEventListener("resize", playVideo);
-    return () => window.removeEventListener("resize", playVideo);
-  }, [videoSrc]);
   return (
     <div className="video-container">
-      <video ref={videoRef} autoPlay loop muted className="video">
+      <ReactPlayer
+        url={videoSrc}
+        width="100%"
+        height="100%"
+        loop
+        muted
+        playing
+      />
+      {/* <video autoPlay loop muted className="video">
         <source src={videoSrc} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      </video> */}
     </div>
   );
 };
