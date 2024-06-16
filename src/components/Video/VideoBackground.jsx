18cbdeb7b8689a71ec986471e80e345f111cf482
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-06-16 19:24:02 -0300

    returned to vdeo html player

--- src/components/Video/VideoBackground.jsx
+++ src/components/Video/VideoBackground.jsx
@@ -1,25 +1,14 @@
 // Importa React
 import React from "react";
-import ReactPlayer from "react-player";
 import "./video.css";
 
 // Crea un componente funcional para el video
 const VideoBackground = ({ videoSrc }) => {
   return (
-    <div className="">
-      <div>
-        <ReactPlayer
-          url={videoSrc}
-          width="100%"
-          height="100%"
-          loop
-          muted
-          playing
-        />
-      </div>
-      {/* <video autoPlay loop muted className="video">
+    <div className="video-container">
+      <video autoPlay loop muted className="video">
         <source src={videoSrc} type="video/mp4" />
-      </video> */}
+      </video>
     </div>
   );
 };
