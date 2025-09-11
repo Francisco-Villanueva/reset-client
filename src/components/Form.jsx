2204c9ae6f7c8308f33612f567eb32271cb9c852
Francisco Villanueva <francisco.villanueva@globant.com>
2025-09-11 10:18:14 -0300

    Integrate agenda pro embeded form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,16 +2,10 @@ import Layout from "../commons/Layout";
 export default function Form() {
   return (
     <Layout
-      className=" bg-white relative h-[100%]  flex justify-center items-center gap-4  pt-14 max-md:pt-0   "
+      className=" bg-white relative h-[180vh]  flex justify-center items-center gap-4  pt-14 max-md:pt-0   "
       id={"form"}
     >
-      <div>
-        <p className="text-xl w-96 text-center">
-          Aproximadamente a las <strong> 14:00 hs </strong> volverá a estar
-          disponible el sistema de turnos
-        </p>
-      </div>
-      {/* <iframe
+      <iframe
         src="https://agendapro.com/iframe/overview/40e834df-51fa-4dbc-9fed-9a7d9398e544"
         width="810"
         height={"100%"}
@@ -21,7 +15,7 @@ export default function Form() {
         scrolling="yes"
         className="border-0"
         // style="border-width:0px;"
-      ></iframe> */}
+      ></iframe>
     </Layout>
   );
 }
